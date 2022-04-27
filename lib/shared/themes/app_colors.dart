7536da634392345b4225f71bfc50fbba78be0368
joaoHenriqueMachado <joaohenriquepereiramachado@gmail.com>
2022-04-27 17:41:04 -0300

    chore: Estilização da paleta de cores. Estilização dos textos. Criação da tela inicial do aplicativo.

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF2463A4);
+  static const secondary = Color(0xFFDB3B4A);
+  static const gray = Color(0xFF585666);
+  static const textPrimary = Color(0xFF2B2B32);
+  static const textCaption = Color(0xFF585666);
+  static const buttons = Color(0xFFF6F6F6);
+  static const background = Color(0xFFF6F6F6);
+}
---
+++ lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:izi_bus/shared/themes/app_colors.dart';
+
+class TextStyles {
+  static final cardTitle = GoogleFonts.outfit(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+  static final cardCaption = GoogleFonts.outfit(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textCaption,
+  );
+  static final button = GoogleFonts.outfit(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.buttons,
+  );
+}
