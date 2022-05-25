26e90dfdf5d56cc9ed225bd150b376cc842222f1
danielg-favero <danielfavero17@gmail.com>
2022-05-24 22:39:04 -0300

    chore: adicionado ícones customizados na inicialização do mapa

--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:izi_bus/modules/components/stops_temp.dart';
+import 'package:izi_bus/modules/components/buses.temp.dart';
+import 'package:izi_bus/modules/components/stops.temp.dart';
 import 'package:izi_bus/shared/themes/app_colors.dart';
 import 'package:izi_bus/shared/themes/app_text_styles.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:izi_bus/utils/custom_marker_icon.dart';
 
 // ignore: use_key_in_widget_constructors
 class Home extends StatefulWidget {
@@ -12,15 +14,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final List<Marker> _markers = <Marker>[];
+  // Markers do mapa
+  final List<Marker> stopsMarkers = <Marker>[];
+  final List<Marker> busesMarkers = <Marker>[];
+  final List<Marker> mapMarkers = <Marker>[];
 
+  // Posição inicial do mapa
   static const _initialCameraPosition = CameraPosition(
     target: LatLng(-26.2295, -52.6716),
     zoom: 15,
   );
 
   final Completer<GoogleMapController> _controller = Completer();
-
   late GoogleMapController _googleMapController;
 
   Future<void> _setMapInitialPosition() async {
@@ -29,12 +34,29 @@ class _HomeState extends State<Home> {
         .animateCamera(CameraUpdate.newCameraPosition(_initialCameraPosition));
   }
 
-  void _addInitialMarkers() {
-    stops.forEach((stop) {
-      _markers.add(Marker(
-          markerId: MarkerId(stop['id']),
-          position: LatLng(stop['lat'], stop['long'])));
-    });
+  Future<void> _addInitialStopsMarkers() async {
+    for (var stop in stops) {
+      stopsMarkers.add(Marker(
+        markerId: MarkerId(stop['id']),
+        icon: BitmapDescriptor.fromBytes(
+            await customMarkerIcon('lib/assets/pin_icon.png', 80)),
+        position: LatLng(stop['lat'], stop['long']),
+      ));
+    }
+
+    setState(() {});
+  }
+
+  Future<void> _addInitialBusesMarkers() async {
+    for (var bus in buses) {
+      busesMarkers.add(Marker(
+        markerId: MarkerId(bus['id']),
+        icon: BitmapDescriptor.fromBytes(
+            await customMarkerIcon('lib/assets/bus_icon.png', 80)),
+        position: LatLng(bus['lat'], bus['long']),
+      ));
+    }
+
     setState(() {});
   }
 
@@ -60,8 +82,10 @@ class _HomeState extends State<Home> {
             onMapCreated: (GoogleMapController controller) {
               _setMapInitialPosition();
               _controller.complete(controller);
+              _addInitialStopsMarkers();
+              _addInitialBusesMarkers();
             },
-            markers: Set<Marker>.of(_markers),
+            markers: Set<Marker>.of(busesMarkers),
           ),
           Padding(
             padding: const EdgeInsets.only(
@@ -69,8 +93,7 @@ class _HomeState extends State<Home> {
             ),
             child: GestureDetector(
               onTap: () => {
-                Navigator.of(context)
-                    .pushNamed('/lines_page', arguments: "Show lines")
+                Navigator.of(context).pushNamed('/lines_page', arguments: '')
               },
               child: Container(
                 decoration: const BoxDecoration(
@@ -93,9 +116,7 @@ class _HomeState extends State<Home> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
         foregroundColor: Colors.black,
-        onPressed: () {
-          _addInitialMarkers();
-        },
+        onPressed: () {},
         child: const Icon(Icons.center_focus_strong),
       ),
     );
