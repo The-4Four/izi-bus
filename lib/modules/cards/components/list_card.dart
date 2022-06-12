bb6208acbc7a81b1fcf2429e227e8dd254d29b16
”Cinglair <cinglair.llo@gmail.com>
2022-06-12 19:03:50 -0300

    Add user card list page

---
+++ lib/modules/cards/components/list_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:izi_bus/shared/themes/app_colors.dart';
+import 'package:izi_bus/shared/themes/app_text_styles.dart';
+
+class ListCard extends StatefulWidget {
+  final String id;
+  final String nameCard;
+  final String value;
+  const ListCard(
+      {Key? key, required this.id, required this.nameCard, required this.value})
+      : super(key: key);
+
+  @override
+  State<ListCard> createState() => _ListCardState();
+}
+
+class _ListCardState extends State<ListCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(right: 8.0),
+                child: Icon(Icons.add_card_outlined),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(widget.nameCard, style: TextStyles.mediumBoldText),
+                  Text(widget.id, style: TextStyles.cardCaption),
+                ],
+              ),
+            ],
+          ),
+          Text("RS " + widget.value, style: TextStyles.smallText),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/modules/register_card/register_card.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:izi_bus/modules/components/button/button.dart';
+import 'package:izi_bus/modules/components/text_input/text_input.dart';
+import 'package:izi_bus/shared/themes/app_colors.dart';
+import 'package:izi_bus/shared/themes/app_text_styles.dart';
+
+class RegisterCard extends StatefulWidget {
+  const RegisterCard({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterCard> createState() => _RegisterCardState();
+}
+
+String? _validarNome(String? value) {
+  String patttern = r'(^[a-zA-Z ]*$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value?.length != null) {
+    return "Informe o nome";
+  } else if (!regExp.hasMatch(value!)) {
+    return "O nome deve conter caracteres de a-z ou A-Z";
+  }
+  return null as String;
+}
+
+class _RegisterCardState extends State<RegisterCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.background,
+        body: Padding(
+          padding:
+              const EdgeInsets.only(top: 72, left: 24, right: 24, bottom: 40),
+          child: Column(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    child: const Icon(Icons.arrow_back, size: 32),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 32),
+                    child: RichText(
+                        text: TextSpan(
+                            text: "Informe os dados do cartão",
+                            style: TextStyles.title)),
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextInput(
+                            placeholder: "Seu Cpf",
+                            validator: _validarNome,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextInput(
+                            placeholder: "ID do cartão",
+                            validator: _validarNome,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextInput(
+                            placeholder: "Nome do cartão",
+                            validator: _validarNome,
+                          ),
+                        ),
+                      ]),
+                ],
+              ),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Button(
+                    text: "Cadastrar Cartão",
+                    onPressed: () {},
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
